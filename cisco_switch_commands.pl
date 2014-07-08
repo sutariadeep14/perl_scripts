402268266bb4d503e4761f720d8ce439e845a258
sutariadeep <deep@Deeps-MacBook-Pro.local>
2014-07-07 23:51:11 -0700

    First commit
    Cisco switch commands, e.g.: show mac-address

---
+++ cisco_switch_commands.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+use strict;
+use Net::Telnet::Cisco;
+use Data::Dumper;
+
+my $host = 'Enter switch IP';
+
+my $session = Net::Telnet::Cisco->new(Host => $host, -Prompt=>'/(?m:^[\w.&-]+\s?(?:\(config[^\)]*\))?\s?[\$#>]\s?(?:\(enable\))?\s*$)/');
+
+$session->login(Name => 'admin',Password => 'password');
+
+#my @ver = $session->cmd('show mac-address-table');
+$session->cmd('terminal length 0');
+my @ver = $session->cmd('show mac-address-table dynamic');
+#print @ver;
